e3f1be8ac077742406842eab3db17d58af6a3e73
Egle <132908296+EgleUrbone@users.noreply.github.com>
2023-08-14 15:59:24 +0300

    CallToAction init

---
+++ src/components/CallToAction.jsx
@@ -0,0 +1,17 @@
+import MainBtn from "./MainBtn";
+
+function CallToAction() {
+  return (
+    <div>
+      <div>
+        <h2>The fastest way from idea to live site. Period.</h2>
+        <p>Flex is a Small SaaS Business. Flex isn’t a traditional company.</p>
+      </div>
+      <div>
+        <MainBtn text={'Get Started'} />
+      </div>
+    </div>
+  )
+}
+
+export default CallToAction;
--- src/components/pointsSection/PointsList.jsx
+++ src/components/pointsSection/PointsList.jsx
@@ -48,7 +48,7 @@ export default function PointsList() {
   ];
 
   return (
-    <div className="grid grid-cols-3 mx-36 gap-x-8">
+    <div className="grid grid-cols-3 mx-36 gap-x-8 mb-24">
       {pointsArr.map((pObj) => (
         <SinglePoint key={pObj.id} item={pObj} />
       ))}
